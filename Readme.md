371643707322cd7c5e1691a1fa69ed43c2df4bf2
yunyanan <yunyanan1@gmail.com>
2020-02-04 16:49:26 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+This is my emacs configuration, it's simple, but in terms of editing, I think
+it's good enough. Of course, I will continue to improve it.
---
+++ init.el
@@ -0,0 +1,26 @@
+;; Make startup faster by reducing the frequency of garbage collection.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+(require 'package)
+(package-initialize)
+
+(if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
+    (load-file (expand-file-name "config.el" user-emacs-directory))
+  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+
+;; Make gc pauses faster by decreasing the threshold.
+(setq gc-cons-threshold (* 2 1000 1000))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-plus-contrib ggtags use-package-ensure-system-package delight use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

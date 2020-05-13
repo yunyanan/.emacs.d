9e6d069d09163045f4e2891942c63f7a91e5bc93
yunyanan <yunyanan@hopeleadtech.com>
2020-05-13 21:02:38 +0800

    commit

--- init.el
+++ init.el
@@ -17,10 +17,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (window-numbering super-save markdown-mode org-plus-contrib ggtags use-package-ensure-system-package delight use-package))))
+	(leetcode window-numbering super-save markdown-mode org-plus-contrib ggtags use-package-ensure-system-package delight use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(snails-content-buffer-face ((t (:background "#111" :height 160))))
+ '(snails-header-line-face ((t (:inherit font-lock-function-name-face :underline t :height 1.3))))
+ '(snails-input-buffer-face ((t (:background "#111" :height 160)))))
